8b8bdae85ccaf37ff589dfc0f075bd88b8e8ad40
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-02-16 02:38:17 +0800

    Update lineage_amethyst.mk

--- lineage_amethyst.mk
+++ lineage_amethyst.mk
@@ -1,11 +1,12 @@
+
 # Inherit from common AOSP/Lineage configs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Herit from LineageOS common product
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk
+# Inherit from LineageOS common product
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from the hardware-specific device.mk (Dito yung display, audio, etc.)
+# Inherit from the hardware-specific device.mk (Display, audio, etc.)
 $(call inherit-product, device/xiaomi/amethyst/device.mk)
 
 # Inherit from the proprietary vendor blobs
@@ -20,16 +21,17 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Fingerprint mula sa screenshot mo (BP2A.250605.031.A3)
+# Build Properties & Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="amethyst" \
     PRODUCT_NAME="amethyst"
-    BUILD_FINGERPRINT := Redmi/amethyst/amethyst:16/BP2A.250605.031.A3/OS3.0.3.0.WOPCNXM:user/release-keys
 
-# Inherit from the proprietary vendor configurations
+BUILD_FINGERPRINT := Redmi/amethyst/amethyst:14/BP2A.250605.031.A3/OS3.0.3.0.WOPCNXM:user/release-keys
+
+# Rootdir - Init scripts & FSTAB
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
+    device/xiaomi/amethyst/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
 # VINTF Manifests
-DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/vintf/manifest.xml
-DEVICE_MATRIX_FILE += $(LOCAL_PATH)/vintf/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += device/xiaomi/amethyst/vintf/manifest.xml
+DEVICE_MATRIX_FILE += device/xiaomi/amethyst/vintf/compatibility_matrix.xml
